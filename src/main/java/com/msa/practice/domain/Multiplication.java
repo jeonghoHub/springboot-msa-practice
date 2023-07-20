1091ee0d075c45f9de8452b985a77cad7ff887c7
usa_develop <tjsrsdqq4@gmail.com>
2023-07-20 21:39:05 +0900

    :sparkles: multiplication 도메인 생성

---
+++ src/main/java/com/msa/practice/domain/Multiplication.java
@@ -0,0 +1,38 @@
+package com.msa.practice.domain;
+
+/**
+ * 애플리케이션에서 곱셈을 나타내는 클래스
+ */
+public class Multiplication {
+
+    private int factorA;
+    private int factorB;
+    private int result;
+
+    public Multiplication(int factorA, int factorB) {
+        this.factorA = factorA;
+        this.factorB = factorB;
+        this.result = factorA * factorB;
+    }
+
+    public int getFactorA() {
+        return factorA;
+    }
+
+    public int getFactorB() {
+        return factorB;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Multiplication{" +
+                "factorA=" + factorA +
+                ", factorB=" + factorB +
+                ", result=" + result +
+                '}';
+    }
+}
