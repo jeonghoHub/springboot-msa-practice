39a74a57bd9d93f9d4427bfe0e0b26d859f1cbc8
usa_develop <tjsrsdqq4@gmail.com>
2023-07-22 15:01:30 +0900

    :recycle:  롬복으로 대체 가능한 메소드들 수정

--- src/main/java/com/msa/practice/domain/Multiplication.java
+++ src/main/java/com/msa/practice/domain/Multiplication.java
@@ -1,38 +1,23 @@
 package com.msa.practice.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
 /**
  * 애플리케이션에서 곱셈을 나타내는 클래스
  */
-public class Multiplication {
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public final class Multiplication {
 
-    private int factorA;
-    private int factorB;
-    private int result;
+    private final int factorA;
+    private final int factorB;
 
-    public Multiplication(int factorA, int factorB) {
-        this.factorA = factorA;
-        this.factorB = factorB;
-        this.result = factorA * factorB;
-    }
-
-    public int getFactorA() {
-        return factorA;
-    }
-
-    public int getFactorB() {
-        return factorB;
-    }
-
-    public int getResult() {
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Multiplication{" +
-                "factorA=" + factorA +
-                ", factorB=" + factorB +
-                ", result=" + result +
-                '}';
+    Multiplication() {
+        this(0, 0);
     }
 }
