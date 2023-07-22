caf2329a4f583f3f6a2518dcba1ad2e01a4e2e97
usa_develop <tjsrsdqq4@gmail.com>
2023-07-22 16:11:23 +0900

    :sparkles: RandomGeneratorService 구현체 생성

--- src/main/java/com/msa/practice/service/RandomGeneratorService.java
+++ src/main/java/com/msa/practice/service/RandomGeneratorService.java
@@ -1,9 +1,21 @@
 package com.msa.practice.service;
 
+import java.util.Random;
+
 public interface RandomGeneratorService {
 
     /**
      * @return 무작위로 만든 11이상 99이하의 인사
      */
     int generateRandomFactor();
+
+    class RandomGeneratorServiceImpl implements RandomGeneratorService {
+        final static int MINIMUM_FACTOR = 11;
+        final static int MAXIMUM_FACTOR = 99;
+
+        @Override
+        public int generateRandomFactor() {
+            return new Random().nextInt((MAXIMUM_FACTOR - MINIMUM_FACTOR) + 1) + MINIMUM_FACTOR;
+        }
+    }
 }
