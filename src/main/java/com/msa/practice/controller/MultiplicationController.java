bf1493f4eb947a3144f577d9eeb28db65ab82425
usa_develop <tjsrsdqq4@gmail.com>
2023-07-24 22:31:24 +0900

    :sparkles: MultiplicationController 엔드포인트 추가

--- src/main/java/com/msa/practice/controller/MultiplicationController.java
+++ src/main/java/com/msa/practice/controller/MultiplicationController.java
@@ -1,13 +1,22 @@
 package com.msa.practice.controller;
 
+import com.msa.practice.domain.Multiplication;
 import com.msa.practice.service.MultiplicationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/multiplications")
 public class MultiplicationController {
     private final MultiplicationService multiplicationService;
+
+    @GetMapping("/random")
+    public Multiplication getRandomMultiplication() {
+        return multiplicationService.createRandomMultiplication();
+    }
 
 }
