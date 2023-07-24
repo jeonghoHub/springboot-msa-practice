1cca6066614cda3e2dbcd9d7088fd7ed4cc07f9b
usa_develop <tjsrsdqq4@gmail.com>
2023-07-24 22:31:44 +0900

    :recycle: MultiplicationControllerTest 수정

--- src/test/java/com/msa/practice/controller/MultiplicationControllerTest.java
+++ src/test/java/com/msa/practice/controller/MultiplicationControllerTest.java
@@ -1,10 +1,13 @@
 package com.msa.practice.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.msa.practice.domain.Multiplication;
 import com.msa.practice.service.MultiplicationService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -24,6 +27,12 @@ class MultiplicationControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    private JacksonTester<Multiplication> json;
+
+    @BeforeEach
+    void setUp() {
+        JacksonTester.initFields(this, new ObjectMapper());
+    }
     @Test
     void getRandomMultiplicationTest() throws Exception {
         // given
@@ -38,6 +47,8 @@ class MultiplicationControllerTest {
 
         // then
         assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentAsString())
+                .isEqualTo(json.write(new Multiplication(70, 20)).getJson());
     }
 
 }
